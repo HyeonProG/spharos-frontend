c6556b2dec2a26d4e97b4ec56c89506cc16c4cbe
HyeonProG <guswo3443@naver.com>
2025-03-28 14:07:30 +0900

    feat: meta and ogtag

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import SignInForm from '@/components/pages/auth/SignInForm';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: '로그인페이지',
+};
 
 export default function SignIn() {
   // const handleSignIn = async ( singInFormData: FormData ) => {
